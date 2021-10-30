506808dfa7646c9d7040f5e88f049849847b0c22
songZ <songzmeng@gmail.com>
2021-10-31 01:23:33 +0800

    Update kill-ae.jsx

--- kill-ae.jsx
+++ kill-ae.jsx
@@ -1,4 +1,8 @@
-var command = $.os.indexOf("Win")
-  ? "osascript -e 'tell application \"Terminal\" to do script \"pkill After Effects && killall Terminal\"'"
-  : "taskkill /f /im AfterFX.exe /t";
-system.callSystem(command);
+var command = $.os.indexOf("Win") ?
+    "osascript -e 'tell application \"Terminal\" to do script \"pkill After Effects && killall Terminal\"'" :
+    "taskkill /f /im AfterFX.exe /t";
+if (app.preferences.getPrefAsLong("Main Pref Section", "Pref_SCRIPTING_FILE_NETWORK_SECURITY") === 1) {
+    system.callSystem(command);
+} else {
+    alert("Not Allow Scripts to Write Files and Access Network\nPlease Check Preference")
+};
